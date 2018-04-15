f452d109c235b419d676b225b6b1761b7cb4d473
casrou <casper.nc97@gmail.com>
2018-04-15 11:25:35 +0200

    Changed site.css filename to force cache refresh

--- MedicinSkema/App_Start/BundleConfig.cs
+++ MedicinSkema/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace MedicinSkema
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site_1.css"));
         }
     }
 }
